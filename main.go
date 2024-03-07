6aab61a0d96734f71580d6bbbeec538c378ab8ae
dustin-ward <wardddustin@gmail.com>
2024-03-07 09:34:34 -0500

    colour term check

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/dustin-ward/termtyping/internal/app"
 	"github.com/dustin-ward/termtyping/internal/config"
+	"github.com/muesli/termenv"
 )
 
 var zen_flag bool
@@ -21,6 +23,13 @@ func main() {
 		ZenMode: zen_flag,
 	}
 	app.Config = config
+
+	// Ensure colour terminal is available
+	colour_profile := termenv.ColorProfile()
+	if colour_profile > termenv.ANSI {
+		fmt.Println("WARNING: no colour terminal was detected. This application depends on colour support.\nPress 'enter' to continue...")
+		fmt.Scanln()
+	}
 
 	p := tea.NewProgram(app.NewAppModel(app.StateDefault, nil))
 	if _, err := p.Run(); err != nil {
